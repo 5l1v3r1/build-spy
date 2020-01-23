ff2b159ef2905a4e2ee1059fa9a04e6fbff26699
Osiris <jamessmith1@outlook.com>
2020-01-23 09:33:26 +0000

    Update build-spy.ps1
    Added Software Check
    Cleaned up Formatting

--- build-spy.ps1
+++ build-spy.ps1
@@ -1,13 +1,16 @@
 Clear-Host
+$getDate = Get-Date
+
+
 "---------------------------------------------------------------------------------" | Tee-Object -FilePath .\Build-Spy.Report
 "Build-Spy V0.1 - Quick and Dirty Build Review Script By James Smith (@osiris2600)" | Tee-Object -FilePath .\Build-Spy.Report -Append
+"Script started at $getDate" | Tee-Object -FilePath .\Build-Spy.Report -Append
 "" | Tee-Object -FilePath .\Build-Spy.Report -Append  
 "" | Tee-Object -FilePath .\Build-Spy.Report -Append 
 "" | Tee-Object -FilePath .\Build-Spy.Report -Append 
 "General System Information" | Tee-Object -FilePath .\Build-Spy.Report -Append
 "---------------------------------------------------------------------------------" | Tee-Object -FilePath .\Build-Spy.Report -Append 
 Get-ComputerInfo -property 'CsName','CsDomain','WindowsProductName','WindowsEditionId','WindowsCurrentVersion','WindowsVersion','WindowsRegisteredOrganization','CsDNSHostName','CSDdomain','CSDomainRole','CsAdminPasswordStatus','CsManufacturer','CsModel','CsPrimaryOwnerName','OsEncryptionLevel','LogonServer' | Format-List | Out-File .\Build-Spy.Report -Append
-Write-Output "" 
 Write-Host "*DONE*" -ForegroundColor Black -BackgroundColor Green
 Write-Output ""
 Write-Output "---------------------------------------------------------------------------------" | Tee-Object -FilePath .\Build-Spy.Report -Append
@@ -43,7 +46,7 @@ Write-Output ""
 Write-Output "---------------------------------------------------------------------------------" | Tee-Object -FilePath .\Build-Spy.Report -Append
 Write-Output "Audit Policy Configuration" | Tee-Object -FilePath .\Build-Spy.Report -Append
 Write-Output "---------------------------------------------------------------------------------" | Tee-Object -FilePath .\Build-Spy.Report -Append
-auditpol.exe /get /category:* | Out-File .\Build-Spy.report -Append
+auditpol.exe /get /category:* | findstr /r /v "^$" | Out-File .\Build-Spy.report -Append
 Write-Host "*DONE*" -ForegroundColor Black -BackgroundColor Green
 Write-Output ""
 Write-Output "---------------------------------------------------------------------------------" | Tee-Object -FilePath .\Build-Spy.Report -Append
@@ -52,20 +55,26 @@ Write-Output "------------------------------------------------------------------
 $uacquery = REG QUERY HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Policies\System\ /v EnableLUA
 if ($uacquery -match "0x1")
 {
-    Write-Output "UAC is Enabled" | Out-File .\Build-Spy.Report -Append
+    Write-Output "`r`n***UAC is Enabled***`r`n" | Out-File .\Build-Spy.Report -Append
 }
 else {
-    Write-Output "UAC is Disabled" | Out-File .\Build-Spy.Report -Append
+    Write-Output "`r`n***UAC is Disabled***`r`n" | Out-File .\Build-Spy.Report -Append
 }
 Write-Host "*DONE*" -ForegroundColor Black -BackgroundColor Green
 Write-Output ""
 Write-Output "---------------------------------------------------------------------------------" | Tee-Object -FilePath .\Build-Spy.Report -Append
 Write-Output "Plaintext Passwords in Registry" | Tee-Object -FilePath .\Build-Spy.Report -Append
 Write-Output "---------------------------------------------------------------------------------" | Tee-Object -FilePath .\Build-Spy.Report -Append
-reg query HKLM /f password /t REG_SZ /s | Out-File .\Build-Spy.report -Append
-reg query HKCU /f password /t REG_SZ /s | Out-File .\Build-Spy.report -Append
+reg query HKLM /f password /t REG_SZ /s | findstr /r /v "^$" | Out-File .\Build-Spy.report -Append
+reg query HKCU /f password /t REG_SZ /s | findstr /r /v "^$" | Out-File .\Build-Spy.report -Append
+Write-Host "*DONE*" -ForegroundColor Black -BackgroundColor Green
+Write-Output ""
+Write-Output "---------------------------------------------------------------------------------" | Tee-Object -FilePath .\Build-Spy.Report -Append
+Write-Output "Installed Software" | Tee-Object -FilePath .\Build-Spy.Report -Append
+Write-Output "---------------------------------------------------------------------------------" | Tee-Object -FilePath .\Build-Spy.Report -Append
+wmic product get name,version | findstr /r /v "^$" | Out-File .\Build-Spy.Report -Append
 Write-Host "*DONE*" -ForegroundColor Black -BackgroundColor Green
 Write-Host ""
-Write-Host "Script completed, please see your Build-Spy Report"
+Write-Host "Script completed at $getdate, Please see your Build-Spy Report"
 notepad.exe .\Build-Spy.Report
 Write-Host ""
